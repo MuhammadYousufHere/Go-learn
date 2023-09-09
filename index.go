2a34f75f8a5032da5bd6060f65077b5ed0c57593
Muhammad Yousuf <khanyousufmy80@gmail.com>
2023-09-09 17:16:20 +0500

    basic go lang - func and var

---
+++ index.go
@@ -0,0 +1,13 @@
+package main
+
+var greet string = "Hello Yousuf"
+var age int = 25
+
+func main() {
+	// Variables
+	welcone := "Welcome to go!" //short variable declearation
+
+	println("Hello world")
+	println(welcone)
+	println(greet)
+}
